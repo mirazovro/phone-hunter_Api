2ce347f28d3bfa91df9026313df0e9b699c3d2ed
unknown <mirazovro@gmail.com>
2022-08-30 09:43:49 +0600

    dynimically-phone-added

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
-const loadPhones = async () => {
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+const loadPhones = async (searchText) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url);
     const data = await res.json();
     displayPhones(data.data)}
 
 
 const displayPhones = phones => {
-    console.log(phones)
+    // console.log(phones)
+    
     const phoneContainer = document.getElementById('phone-container');
-    phones.forEach(phone=>{
-    const phoneDiv = document.createElement('div')
-    phoneDiv.classList.add('col')
-    phoneDiv.innerHTML = `
-                       <div class="card">
+    phoneContainer.innerText = '';
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div')
+        phoneDiv.classList.add('col')
+        phoneDiv.innerHTML = `
+            <div class="card p-4">
                         <img src="${phone.image}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${phone.phone_name}</h5>
@@ -23,13 +25,16 @@ const displayPhones = phones => {
     
     
     
-    
     `
-    phoneContainer.appendChild(phoneDiv)
+        phoneContainer.appendChild(phoneDiv)
     })
     
-    
-}
 
+    document.getElementById('btn-search').addEventListener('click', function () {
+        const searchField = document.getElementById('search-field');
+        const searchText = searchField.value;
+        loadPhones(searchText)
+    })
+}
 
 loadPhones();
